5093ad41974da9702601a29620c5480ef13d0135
Vitor Hugo <vhpinto1@gmail.com>
2022-04-18 15:31:18 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import Tabs from "./components/Tabs.vue";
+import Tab from "./components/Tab.vue";
+</script>
+
+<template>
+    <Tabs>
+      <Tab title="Tab 1">Tab 1 content</Tab>
+      <Tab title="Tab 2">Tab 2 content</Tab>
+      <Tab title="Tab 3">Tab 3 content</Tab>
+      <Tab title="Tab 4">Tab 4 content</Tab>
+    </Tabs>
+</template>
